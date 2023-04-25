1bab332a8950ca3216fe6c3f0bbe569419281a10
dungg211 <118341795+dungg211@users.noreply.github.com>
2023-04-25 22:55:28 +0700

    Update Course.php

--- ManagerStudent/app/Models/Course.php
+++ ManagerStudent/app/Models/Course.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
-{
+{                   
     use HasFactory;
     protected $fillable = ['course_id','subject_id','name_subject','class','teacher_id','name '];
     
